be2188b3cde18ba8e0275256d5e677ec8a17e595
Dubemox <chukwudubemjoseph5@gmail.com>
2026-02-12 08:04:07 +0100

    backend update

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import random
 
 app = FastAPI()
 
-# allow frontend to talk to backend
+origins = [
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
